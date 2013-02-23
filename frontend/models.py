74da8d5ee00c6b27c3e67912b48776a40081d002
fakedarren <darren.waddell@gmail.com>
2013-02-23 22:27:18 +0000

    Use a base template

--- frontend/models.py
+++ frontend/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Page(models.Model):
     url = models.CharField(max_length=50)
     name = models.CharField(max_length=50)
+    template = models.CharField(max_length=50)
     order = models.IntegerField()
     created = models.DateTimeField('date published')
     def __unicode__(self):
--- frontend/views.py
+++ frontend/views.py
@@ -5,7 +5,7 @@ from frontend.models import Page
 def page(request, url):
     menu = Page.objects.order_by('order')
     page = Page.objects.get(url='/' + url)
-    return render_to_response('frontend/page.html', {
+    return render_to_response('frontend/' + page.template + '.html', {
         'menu': menu,
         'page': page
     })
