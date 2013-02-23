116f849d14bc4fb601868b7ae80ba1f0a139a7a9
fakedarren <darren.waddell@gmail.com>
2013-02-23 22:57:31 +0000

    Some static files

--- frontend/views.py
+++ frontend/views.py
@@ -6,6 +6,7 @@ def page(request, url):
     menu = Page.objects.order_by('order')
     page = Page.objects.get(url='/' + url)
     return render_to_response('frontend/' + page.template + '.html', {
+    	'current_url': '/' + url,
         'menu': menu,
         'page': page
     })
